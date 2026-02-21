7c235df7e3b1b59ea6f3480e01e7dee99d65b841
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-21 01:45:35 -0500

    add SumOfSquares.java

---
+++ SumOfSquares.java
@@ -0,0 +1,16 @@
+package basics;
+
+
+/*	Input : n = 2
+	Output: 5
+	Explanation: 1^2+2^2 = 5
+	
+	Input : n = 8
+	Output: 204
+	Explanation :  1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2 + 7^2 + 8^2 = 204 */
+
+public class SumOfSquares {
+	
+	// 4 = 1**2 + 2**2 + 3**2 + 4**2 = 30
+
+}
