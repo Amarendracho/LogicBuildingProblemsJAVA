e4d75222638631abae463017f863422a42a82d39
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-21 02:02:06 -0500

    update SumOfSquare

--- SumOfSquares.java
+++ SumOfSquares.java
@@ -1,6 +1,5 @@
 package basics;
 
-
 /*	Input : n = 2
 	Output: 5
 	Explanation: 1^2+2^2 = 5
@@ -9,8 +8,21 @@ package basics;
 	Output: 204
 	Explanation :  1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2 + 7^2 + 8^2 = 204 */
 
+//4 = 1**2 + 2**2 + 3**2 + 4**2 = 30
 public class SumOfSquares {
-	
-	// 4 = 1**2 + 2**2 + 3**2 + 4**2 = 30
 
+	public static int sumOfSqr(int n) {
+		int sum = 0;
+		int square = 1;
+		for (int i = 1; i <= n; i++) {
+			square = i * i;
+			sum += square;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(sumOfSqr(8));
+
+	}
 }
