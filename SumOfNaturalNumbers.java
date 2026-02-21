4890f74e7f44584145d45f25872e20cfdf34aa07
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-20 23:42:54 -0500

    update SumOfNaturalNumbers.java

--- SumOfNaturalNumbers.java
+++ SumOfNaturalNumbers.java
@@ -26,7 +26,7 @@ public class SumOfNaturalNumbers {
 	}
 
 	// Approach 3 Using Recursion -O(n) and O(n) Space
-	public static int sumOfNNums(int n) {
+	public  int sumOfNNums(int n) {
 
 		// base condition
 		if (n == 1)
@@ -45,6 +45,8 @@ public class SumOfNaturalNumbers {
 		//approach 3
 		System.out.println(sumOfNNums(5));
 		
+		SumOfNaturalNumbers sum = new SumOfNaturalNumbers();
+		System.out.println(sum.sumOfNNums(5));
 		
 	}
 
