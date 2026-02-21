287729e4587f66b8fefe73f593c642acab46ae87
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-20 23:26:18 -0500

    update SumOfNaturalNumbers.java

--- SumOfNaturalNumbers.java
+++ SumOfNaturalNumbers.java
@@ -1,5 +1,16 @@
 package basics;
 
+
+/*	Input: n = 3
+	Output: 6
+	Explanation: 1 + 2 + 3 = 6
+	
+	Input: n = 5
+	Output: 15 
+	Explanation:  1 + 2 + 3 + 4 + 5 = 15*/
+
 public class SumOfNaturalNumbers {
+	
+	
 
 }
