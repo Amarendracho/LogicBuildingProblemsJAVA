23b6fb65ce7b265dc43258a094b3617b4a3c3d6c
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-21 15:45:58 -0500

    add ClosestNumber

---
+++ ClosestNumber.java
@@ -0,0 +1,16 @@
+package basics;
+
+/*Input: n = 13, m = 4
+Output: 12
+Explanation: 12 is the closest to 13, divisible by 4.
+
+Input: n = -15, m = 6
+Output: -18
+Explanation: Both -12 and -18 are closest to -15, but -18 has the maximum absolute value.*/
+
+public class ClosestNumber {
+
+	static void closeNum() {
+
+	}
+}
