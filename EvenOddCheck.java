e298f99077110234eb3b99c15870068cf8e4c4d8
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-20 23:07:44 -0500

    update EvenOddCheck.java

--- EvenOddCheck.java
+++ EvenOddCheck.java
@@ -9,13 +9,14 @@ package basics;
 	Explanation: 44 % 2 = 0, so 44 is even.*/
 
 public class EvenOddCheck {
+	
 	static boolean evenOddCheck(int n) {
 		if (n % 2 == 1)
 			return false;
 		return true;
 
 	}
-
+	// Approach 2
 	static boolean isEven(int n) {
 		int rem = n % 2;
 
@@ -29,6 +30,7 @@ public class EvenOddCheck {
 	public static void main(String[] args) {
 		System.out.println(evenOddCheck(44));
 		
+		// Approach2 
 		int number = 15;
 		if(isEven(number) == true) {
 			System.out.println("true");
