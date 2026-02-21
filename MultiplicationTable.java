465e7a1d725f4c877cf27d2eeb2b63ce6c13bc6a
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-20 23:22:12 -0500

    update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -15,6 +15,7 @@ package basics;
 
 public class MultiplicationTable {
 
+	// Iterative Approach
 	public static void mulTable(int n) {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(n + " * " + i + " = " + n * i);
