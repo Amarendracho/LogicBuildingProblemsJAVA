4f8cfb99cb89d869fc1977aa4373b0fa420db09a
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-21 02:12:56 -0500

    update SumOfSquare

--- SumOfSquares.java
+++ SumOfSquares.java
@@ -36,6 +36,8 @@ public class SumOfSquares {
 		return (n * (n + 1) *
 				(2 * n + 1)) / 6; 
 	}
+	
+	// We can use this formula Too (n * (n + 1) / 2) * (2 * n + 1) / 3;
 
 	public static void main(String[] args) {
 		System.out.println(sumOfSqr(8));
