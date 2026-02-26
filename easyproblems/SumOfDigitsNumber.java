f34673cf9dd3c6b4f266c5f375867b72f595eb6c
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-26 02:11:27 -0500

    update SumOfDigitsNumber.java

--- easyproblems/SumOfDigitsNumber.java
+++ easyproblems/SumOfDigitsNumber.java
@@ -1,5 +1,6 @@
 package easyproblems;
 
+
 /*	Input: n = 687
 	Output: 21
 	Explanation: The sum of its digits are: 6 + 8 + 7 = 21
@@ -33,10 +34,24 @@ public class SumOfDigitsNumber {
 
 	}
 
+	// String Conversion 
+	static int sumOfDigits(int n) {
+
+		String convert = Integer.toString(n);
+		int sum = 0;
+
+		for (char ch : convert.toCharArray()) {
+			sum += ch - '0';
+		}
+		return sum;
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println(sumOfNumbers(6756));
-		
+
 		System.out.println(sumOfNums(129));
+		
+		System.out.println(sumOfDigits(132));
 	}
 }
