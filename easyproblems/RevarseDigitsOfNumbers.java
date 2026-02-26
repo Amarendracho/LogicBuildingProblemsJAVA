b01dc7a22e354840ff9e778b6f289906518c79eb
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-26 02:37:44 -0500

    update RevarseDigitsOfNumbers.java

--- easyproblems/RevarseDigitsOfNumbers.java
+++ easyproblems/RevarseDigitsOfNumbers.java
@@ -14,19 +14,36 @@ package easyproblems;
 
 public class RevarseDigitsOfNumbers {
 
-	//Reversing Digit by Digit - Time Complexity - O(log n) Space Complexity - O(1)
+	// Reversing Digit by Digit - Time Complexity - O(log n) Space Complexity - O(1)
 	public static int revarseDigits(int n) {
 
 		int result = 0;
 		while (n > 0) {
 			result = (result * 10) + (n % 10);
 			n /= 10;
-		} 
+		}
 		return result;
+	}
+
+	// Using String - O(log n) Space Complexity - O(1)
+	static int reversDigits(int n) {
+		// converting number to string
+		StringBuffer s = new StringBuffer(String.valueOf(n));
+
+		// reversing the string
+		s.reverse();
+
+		// converting string to integer
+		n = Integer.parseInt(String.valueOf(s));
+
+		// returning integer
+		return n;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(revarseDigits(12345));
+		
+		System.out.println(reversDigits(12345));
 	}
 
 }
